17edcf253e41dec169fed53f41871f31584ae387
Allen Tan <algtan.dev@gmail.com>
2021-06-09 14:10:44 -0700

    Create index of token icons

---
+++ src/images/tokenIndex.ts
@@ -0,0 +1,19 @@
+import bchToken from './tokens/BCH-token-scalable.svg'
+import btcToken from './tokens/BTC-token-scalable.svg'
+import dogeToken from './tokens/DOGE-token-scalable.svg'
+import ethToken from './tokens/ETH-token-scalable.svg'
+import fioToken from './tokens/FIO-token-scalable.svg'
+import ltcToken from './tokens/LTC-token-scalable.svg'
+import xmrToken from './tokens/XMR-token-scalable.svg'
+import xrpToken from './tokens/XRP-token-scalable.svg'
+
+export const tokenImages = {
+  BCH: bchToken,
+  BTC: btcToken,
+  DOGE: dogeToken,
+  ETH: ethToken,
+  FIO: fioToken,
+  LTC: ltcToken,
+  XMR: xmrToken,
+  XRP: xrpToken
+}
