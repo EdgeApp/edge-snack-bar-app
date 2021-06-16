0ecc342224cd364933b582b7ced7b199b444ff80
Allen Tan <algtan.dev@gmail.com>
2021-06-16 10:35:47 -0700

    Add Header and body styling to MainScene

--- src/MainScene.tsx
+++ src/MainScene.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 
+import { Header } from './components/Header'
 import { currencies, fetchExchangeRates } from './exchangeRate'
+
+const bodyStyle = {
+  margin: '0px',
+  padding: '0px',
+  height: '100%'
+}
 
 const rateFetchDelay = 600000 // Variable to set delay for fetching exchange rates (milliseconds)
 
@@ -24,12 +31,15 @@ export function MainScene(): JSX.Element {
     if (isInitialRender.current) {
       isInitialRender.current = false // Set isInitialMount to false
 
+      Object.assign(document.body.style, bodyStyle) // Update styling for 'body'
+
       updateExchangeRates()
     }
   }, [usdToCoinRates])
 
   return (
     <>
+      <Header />
       <h1>Edge Snack Bar</h1>
       {Object.keys(currencies)
         .sort((a, b) => a.localeCompare(b))
